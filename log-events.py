33bb8e6c83ac828c201efe50b259f1d755e0cd3f
Manohar Mikkili <mmikkili@avaya.com>
2024-02-26 18:57:53 +0530

    new

--- log-events.py
+++ log-events.py
@@ -1,18 +1,18 @@
-import os
-from azure.eventgrid import EventGridConsumer
+import logging
+import json
+import azure.functions as func
 
-# Set your Azure Event Grid topic endpoint and key
-topic_endpoint = os.environ["https://mycustomtopicmmikkili.eastus2-1.eventgrid.azure.net/api/events"]
-topic_key = os.environ["tIoSa+X9Loq/IA8CDEdYz3jwagYZZJrqVT9EXleymWQ="]
+app = func.FunctionApp()
 
-# Create an Event Grid consumer
-consumer = EventGridConsumer()
+@app.function_name(name="eventGridTrigger")
+@app.event_grid_trigger(arg_name="event")
+def eventGridTest(event: func.EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
 
-# Define a callback function to handle incoming events
-def handle_event(event):
-    # Log the event data
-    print("Received event:")
-    print(event)
-
-# Start consuming events from the subscription
-consumer.consume_events(topic_endpoint, topic_key, handle_event)
+    logging.info('Python EventGrid trigger processed an event: %s', result)
